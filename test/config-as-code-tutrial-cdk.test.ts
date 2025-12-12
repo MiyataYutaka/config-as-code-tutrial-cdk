7474c4f2188ce37b652c01bb180b3c3c97925b45
MiyataYutaka <131413473+MiyataYutaka@users.noreply.github.com>
2025-12-12 20:18:55 +0900

    initial commit

---
+++ test/config-as-code-tutrial-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ConfigAsCodeTutrialCdk from '../lib/config-as-code-tutrial-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/config-as-code-tutrial-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ConfigAsCodeTutrialCdk.ConfigAsCodeTutrialCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
